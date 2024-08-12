febfed4e6475a9c289bc99db20fc523bdcc3fc27
SamkeMas <maslic.sam@gmail.com>
2024-08-12 13:29:54 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Express } from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import todoRoutes from "./routes"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(express.json())
+app.use(todoRoutes)
+
+
+const uri: string = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URI}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
+
+mongoose
+  .connect(uri)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://${uri}:${PORT}`)
+    )
+  )
+  .catch(error => {
+    throw error
+  })
